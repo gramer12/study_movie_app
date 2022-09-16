3b3205f6fe65440263daf9fae4f9b6029f8d519d
gramer12 <yid05011@naver.com>
2022-09-16 18:02:55 +0900

    react init

--- src/App.js
+++ src/App.js
@@ -1,18 +1,53 @@
+import { render } from "@testing-library/react";
 import React from "react";
-import Food from "./Food";
 
-const favArr = ["김치", "볶음", "파전"];
-function App() {
-  return (
-    <React.Fragment>
-      <h1 id="one">좋아하는 음식 목록</h1>
-      <ul>
-        {favArr.map((ele) => {
-          return <Food fav={ele} />;
-        })}
-      </ul>
-    </React.Fragment>
-  );
+//state는 동적데이터를 다룰때 사용하는 리엑트요소
+
+// function App() {
+//   return <div>함수 컴포넌트 </div>;
+// }
+
+// class App extends React.Component {
+//   render() {
+//     return <div>클래스 컴포넌트</div>
+//   }
+// }
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0,
+      a: 1,
+    };
+  }
+
+  증가함수 = () => {
+    console.log("증가함수가 호출됨");
+    // this.state.count++;
+    // this.setState({ a: 2 });
+    this.setState((s) => {
+      return { count: s.count + 1 };
+    });
+  };
+  감소함수 = () => {
+    console.log("감소함수가 호출됨");
+    // this.setState({ count: this.state.count - 1 });
+    this.setState((s) => {
+      return { count: s.count - 1 };
+    });
+    console.log(this.state.count);
+  };
+  render() {
+    console.log(this.state);
+    return (
+      <div>
+        <h1>카운터:{this.state.count}</h1>
+        <button onClick={this.증가함수}>+1</button>
+        <button onClick={this.감소함수}>-1</button>
+      </div>
+    );
+  }
 }
 
 export default App;
